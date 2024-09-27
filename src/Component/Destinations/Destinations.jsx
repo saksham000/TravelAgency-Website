82bb32384812b43c4effce0c3586897ec727f340
Saksham Sharma <sakshamshankyan7@gmail.com>
2024-09-27 21:08:30 +0530

    MediaQueryies Done

--- src/Component/Destinations/Destinations.jsx
+++ src/Component/Destinations/Destinations.jsx
@@ -71,7 +71,7 @@ function Destinations() {
   return (
     <div className="destination section container">
       <div className="secContainer">
-        <div className="">
+        <div className="secTitle">
           <span className="redText">EXPLORE NOW</span>
           <h3>Find Your Dream Destination</h3>
           <p>
--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,30 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Home.css";
 import { AiOutlineSwapRight } from "react-icons/ai";
 import image1 from "../../Assets/image1.jpg";
 
 import Video from "../../Assets/video.mp4";
 
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 function Home() {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
   return (
     <div className="Home">
       <div className="videoBg">
         <video src={Video} autoPlay loop muted></video>
       </div>
       <div className="sectionText">
-        <h1>Unlock Your Travel Dreams with us !</h1>
-        <p>
+        <h1 data-aos="fade-up">Unlock Your Travel Dreams with us !</h1>
+        <p data-aos="fade-up">
           Discover the world's most adverturose nature, lifeis so short for a
           trip.
         </p>
-        <button className="btn flex">
+        <button className="btn flex" data-aos="fade-up">
           Get STarted <AiOutlineSwapRight className="icon" />
         </button>
       </div>
       <div className="popularPlaces">
         <div className="content">
-          <h3>Popular Places</h3>
-          <div className="images flex">
+          <h3 data-aos="fade-up">Popular Places</h3>
+          <div className="images flex" data-aos="fade-up">
             <img src={image1} alt="" />
             <img src={image1} alt="" />
             <img src={image1} alt="" />
